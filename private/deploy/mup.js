690b6a3c9e87e2a8e033ac7c996713bd12fef00a
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2016-04-09 14:50:40 +0700

    change smt

--- private/deploy/mup.js
+++ private/deploy/mup.js
@@ -8,7 +8,7 @@ module.exports = {
     },
 
     meteor: {
-        name: 'tubechat',
+        name: 'tubechat2',
         path: '/D/Projects/xxx/tubechat2',
         servers: {
             one: {
@@ -23,13 +23,5 @@ module.exports = {
             MONGO_URL : "mongodb://gnocxn:123456978@ds028679.mlab.com:28679/tubechat"
         },
         deployCheckWaitTime: 70 //default 10
-    },
-
-   /* mongo: { //optional
-        oplog: true,
-        port: 27017,
-        servers: {
-            one: {},
-        },
-    },*/
+    }
 };
