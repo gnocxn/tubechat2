a90ba1428a8a922fdd8d47bb6cb7243104f35e03
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2016-04-09 14:19:42 +0700

    80%

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+let ModelSubs = new SubsManager();
+
 Template.modelList.onCreated(function () {
 	let self = this;
 	self.loaded = new ReactiveVar(0);
@@ -9,7 +11,7 @@ Template.modelList.onCreated(function () {
 		is_online: true,
 		current_show: {$ne: 'private'}
 	});
-
+	self.ready = new ReactiveVar(false);
 
 
 	let options = {
@@ -21,33 +23,62 @@ Template.modelList.onCreated(function () {
 	}
 
 	self.autorun(function (c) {
-
-		let gender = FlowRouter.getParam('gender');
-		let tag = FlowRouter.getParam('tag');
+		self.filter.set({
+			is_online: true,
+			current_show: {$ne: 'private'}
+		});
+		//self.ready.set(false);
+		FlowRouter.watchPathChange();
+		let currentContext = FlowRouter.current();
+		let gender = (currentContext && currentContext.params.gender) ? currentContext.params.gender : null;
+		let tag = (currentContext && currentContext.params.tag) ? currentContext.params.tag : null;
+		let _filter = self.filter.get();
 
 		if (gender) {
-			let _filter = self.filter.get();
 			_filter = _.extend(_filter, {gender: gender});
-			self.filter.set(_filter);
 		}
 
-		if (tag) {
-			let _filter = self.filter.get();
+		if(tag){
 			_filter = _.extend(_filter, {tags: tag});
-			self.filter.set(_filter);
 		}
+
 		let limit = self.limit.get();
-		let _filter = self.filter.get();
+
 		options = _.extend(options, {limit: limit});
-		let subscription = self.subscribe('models.list', _filter, options);
+		let subscription = ModelSubs.subscribe('models.list', _filter, options);
 		if(subscription.ready()){
+			self.ready.set(true);
 			self.loaded.set(limit);
 		}
+
+		//console.log(_filter);
+
+		/*		let gender = _gender.get();
+		 let tag = _tag.get();
+
+		 if (gender) {
+		 let _filter = self.filter.get();
+		 _filter = _.extend(_filter, {gender: gender});
+		 self.filter.set(_filter);
+		 }
+
+		 if (tag) {
+		 let _filter = self.filter.get();
+		 _filter = _.extend(_filter, {tags: tag});
+		 self.filter.set(_filter);
+		 }
+		 let limit = self.limit.get();
+		 let _filter = self.filter.get();
+		 options = _.extend(options, {limit: limit});
+		 let subscription = self.subscribe('models.list', _filter, options);
+		 if(subscription.ready()){
+		 self.loaded.set(limit);
+		 }*/
 	});
 
 	self.models = () => {
 		let _filter = self.filter.get();
-		console.info(_filter);
+		//console.info(_filter);
 		options = _.extend(options, {limit: self.loaded.get()});
 		return Models.find(_filter, options);
 	}
@@ -57,7 +88,10 @@ Template.modelList.helpers({
 	models: function () {
 		return Template.instance().models();
 	},
-	hasLoadMore : function(){
+	isReady : function(){
+		return Template.instance().ready.get();
+	},
+	hasLoadMore: function () {
 		return Template.instance().models().count() >= Template.instance().limit.get();
 	}
 });
@@ -72,8 +106,29 @@ Template.modelList.events({
 });
 
 Template.modelList_Item.helpers({
-	model : function(){
+	model: function () {
 		return Template.instance().data;
 	}
+});
+
+Template.modelSingle.onCreated(function(){
+	let self = this;
+	let username = FlowRouter.getParam('username');
+
+	self.autorun(function(){
+		self.subscribe('models.single',username);
+	});
+
+	self.model = () => {
+		return Models.findOne({username : username});
+	}
+});
+
+Template.modelSingle.helper({
+	model : function(){
+		return Template.instance().model();
+	}
 })
+
+
 
